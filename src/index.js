ad30b238a310407e1e5c94530124a802c5347867
YourGitHubUsername <abhishekbiradar2856@gmail.com>
2025-11-16 23:52:38 +0530

    added .env

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
-const express = require("express")
-const app = express()
-app.use(express.json())
+require("dotenv").config();
+const express = require("express");
+const connectDB = require("./config/database.js");
+const app = express();
+const PORT = 7777;
+app.use(express.json());
+
+app.post("/signup", (req, res) => {});
+
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(`Server connection failed : ${err}`);
+  });
